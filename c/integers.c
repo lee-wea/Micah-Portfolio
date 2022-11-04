0e93bdd9aeb205709c3c11272810b8e4b7c896e2
Micah Weathersby <2023weathersby.micah@fcboe.org>
2022-11-04 10:38:30 -0400

    Added info to the C row

---
+++ c/integers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int x;
+	x = 2 + 4;
+	printf("%i\n", x);
+	x = 6 * 6;
+	printf("%i\n", x);
+	x = 4 * 4 - 2;
+	printf("%i\n", x);
+	x = 12 / 3 - 20;
+	printf("%i\n", x);
+	float y  = 25 / 4;
+	printf("%f\n", y);
+
+	return 0;
+}
