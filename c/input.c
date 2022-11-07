5471875876dc1c9206fb31e504ce4b43570e0ee1
Micah Weathersby <2023weathersby.micah@fcboe.org>
2022-11-07 11:09:26 -0500

    Added input and string files

--- c/input.c
+++ c/input.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	int var;
-	printf("Type in an integer: ");
-	scanf("%i", &var);
-	printf("You typed in %i\n", var);
+	int var, var2;
+	printf("Type in 2 integer: ");
+	scanf("%i%i", &var, &var2);
+	printf("Your sum of the numbers is %i\n", var + var2);
 	return 0;
 }
